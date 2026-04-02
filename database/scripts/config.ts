5fa38af65f4818274f59de9bfe3550c5ec7ae876
Luke <lucas.arrington@gmail.com>
2026-04-02 16:16:14 -0400

    Update Firebase configuration
    Updated Firebase configuration with new credentials.

--- database/scripts/config.ts
+++ database/scripts/config.ts
@@ -1,12 +1,11 @@
 export const firebaseConfig = {
   // Your web app's Firebase configuration here
   // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
-  apiKey: 'API_KEY',
-  authDomain: 'PROJECT_ID.firebaseapp.com',
-  databaseURL: 'https://PROJECT_ID.firebaseio.com',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'PROJECT_ID.appspot.com',
-  messagingSenderId: 'SENDER_ID',
-  appId: 'APP_ID',
-  measurementId: 'G-MEASUREMENT_ID',
+  apiKey: "AIzaSyCEk4W4ASSflQeaxjkHGhZFkUBttMietLU",
+  authDomain: "fir-quickstart-16d0c.firebaseapp.com",
+  projectId: "fir-quickstart-16d0c",
+  storageBucket: "fir-quickstart-16d0c.firebasestorage.app",
+  messagingSenderId: "292493549584",
+  appId: "1:292493549584:web:09b7928a65975d4cdaea95",
+  measurementId: "G-NM0D7RE8NN"
 };
